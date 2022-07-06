547e4e90441a470c04365e7b73a300a89ce878bf
a.kucheryavenkova <a.kucheryavenkova@m-games-ltd.com>
2022-07-06 17:31:54 +0300

    add main screen

--- wp-content/themes/myusa/homepage.php
+++ wp-content/themes/myusa/homepage.php
@@ -9,7 +9,22 @@
 
 <div class="content">
 
-	<div class="main-screen">
+	<div class="main-screen homepage-screen">
+		<div class="main-screen__row container">
+			<div class="inner">
+				<div class="main-screen__row_block">
+					<h1>«Технология климата»</h1>
+					<div class="main-screen__disc">Продажа, монтаж и сервис климатического оборудования</div>
+					<div class="main-screen__btn btn">
+						<button>Выбрать кондиционер</button>
+					</div>
+				</div>
+				<div class="main-screen__row_block"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="main-screen homepage-screen">
 		<div class="main-screen__row container">
 			<div class="inner">
 				<h1>«Технология климата»</h1>
