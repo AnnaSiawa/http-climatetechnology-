cc86197cf0b26fa9f76ac69f17834d74227d0c85
Anna <a.kucheryavenkova@m-games-ltd.com>
2022-08-11 11:07:21 +0300

    add pagination

--- wp-content/themes/myusa/products-page.php
+++ wp-content/themes/myusa/products-page.php
@@ -772,14 +772,133 @@
 										</div>
 									</div>
 								</div>
-<!--								<div class="pagination-wrap">-->
-<!--									<div id="pagination" class="pagination"></div>-->
-<!--								</div>-->
+								<div class="product-swiper__slide">
+									<div class="product-swiper__item item container">
+										<div class="product-swiper__img">
+											<img alt="product-swiper-img"
+												 src="<?php bloginfo('template_url'); ?>/dist/images/conditional2.webp">
+										</div>
+										<div class="product-swiper__content">
+											<div class="product-swiper__title">11 Кондиционер Lanzkraft
+												LSWH-20FC1N/LSAH-20FC1N
+											</div>
+											<div class="product-swiper__disk-wrap">
+												<div class="product-swiper__disk">
+													<div class="product-swiper__disk_title">Основные характеристики
+													</div>
+													<div class="product-swiper__disk_arrow">
+														<img alt="product-swiper-img"
+															 src="<?php bloginfo('template_url'); ?>/dist/images/close-disk.png">
+													</div>
+												</div>
+												<div class="product-swiper__detail">
+													<ul class="product-swiper__list">
+														<li class="product-swiper__item">Тип: сплит-система</li>
+														<li class="product-swiper__item">Размещение внутреннего блока:
+															настенный
+														</li>
+														<li class="product-swiper__item">Площадь помещения:
+															20&nbsp;кв.м
+														</li>
+														<li class="product-swiper__item">Тип фреона: R-410A</li>
+													</ul>
+												</div>
+											</div>
+
+											<div class="product-swiper__disk-wrap">
+												<div class="product-swiper__disk">
+													<div class="product-swiper__disk_title">Размеры</div>
+													<div class="product-swiper__disk_arrow">
+														<img alt="product-swiper-img"
+															 src="<?php bloginfo('template_url'); ?>/dist/images/close-disk.png">
+													</div>
+												</div>
+												<div class="product-swiper__detail">
+													<ul class="product-swiper__list">
+														<li class="product-swiper__item">Высота внутреннего
+															блока/внешнего блока: 27&nbsp;см/45.8&nbsp;см
+														</li>
+														<li class="product-swiper__item">Ширина внутреннего
+															блока/внешнего блока: 72&nbsp;см/63.5&nbsp;см
+														</li>
+														<li class="product-swiper__item">Глубина внутреннего
+															блока/внешнего блока: 20.1&nbsp;см/24&nbsp;см
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="product-swiper__btn btn">
+												<button>Купить за 24 990 ₽</button>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="product-swiper__slide">
+									<div class="product-swiper__item item container">
+										<div class="product-swiper__img">
+											<img alt="product-swiper-img"
+												 src="<?php bloginfo('template_url'); ?>/dist/images/conditional2.webp">
+										</div>
+										<div class="product-swiper__content">
+											<div class="product-swiper__title">11 Кондиционер Lanzkraft
+												LSWH-20FC1N/LSAH-20FC1N
+											</div>
+											<div class="product-swiper__disk-wrap">
+												<div class="product-swiper__disk">
+													<div class="product-swiper__disk_title">Основные характеристики
+													</div>
+													<div class="product-swiper__disk_arrow">
+														<img alt="product-swiper-img"
+															 src="<?php bloginfo('template_url'); ?>/dist/images/close-disk.png">
+													</div>
+												</div>
+												<div class="product-swiper__detail">
+													<ul class="product-swiper__list">
+														<li class="product-swiper__item">Тип: сплит-система</li>
+														<li class="product-swiper__item">Размещение внутреннего блока:
+															настенный
+														</li>
+														<li class="product-swiper__item">Площадь помещения:
+															20&nbsp;кв.м
+														</li>
+														<li class="product-swiper__item">Тип фреона: R-410A</li>
+													</ul>
+												</div>
+											</div>
+
+											<div class="product-swiper__disk-wrap">
+												<div class="product-swiper__disk">
+													<div class="product-swiper__disk_title">Размеры</div>
+													<div class="product-swiper__disk_arrow">
+														<img alt="product-swiper-img"
+															 src="<?php bloginfo('template_url'); ?>/dist/images/close-disk.png">
+													</div>
+												</div>
+												<div class="product-swiper__detail">
+													<ul class="product-swiper__list">
+														<li class="product-swiper__item">Высота внутреннего
+															блока/внешнего блока: 27&nbsp;см/45.8&nbsp;см
+														</li>
+														<li class="product-swiper__item">Ширина внутреннего
+															блока/внешнего блока: 72&nbsp;см/63.5&nbsp;см
+														</li>
+														<li class="product-swiper__item">Глубина внутреннего
+															блока/внешнего блока: 20.1&nbsp;см/24&nbsp;см
+														</li>
+													</ul>
+												</div>
+											</div>
+											<div class="product-swiper__btn btn">
+												<button>Купить за 24 990 ₽</button>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 							<div class="pagination-wrap">
-								<a class="pagination-btn-p"></a>
+								<a data-goto=".scroll-top-catalog" class="pagination-btn-p"></a>
 								<ul id="pagination" class="pagination"></ul>
-								<a class="pagination-btn-n"></a>
+								<a data-goto=".scroll-top-catalog" class="pagination-btn-n"></a>
 							</div>
 						</div>
 					</div>
--- wp-content/themes/myusa/src/js/pagination.js
+++ wp-content/themes/myusa/src/js/pagination.js
@@ -2,8 +2,11 @@ let pagination = document.querySelector('#pagination');
 let slideListsWrap = document.querySelector('.product-swiper__wrapper');
 let slideLists = document.querySelectorAll('.product-swiper__slide');
 let arrSlideLists = Array.from(slideLists);
+let activeLi = document.querySelector('#pagination li.current');
+let prevPage = document.querySelector('.pagination-btn-p');
+let nextPage = document.querySelector('.pagination-btn-n');
 
-let notesOnPage = 1; //количество записей на странице
+let notesOnPage = 2; //количество записей на странице
 let countOfItems = Math.ceil(arrSlideLists.length / notesOnPage); //количество страниц пагинации
 
 let items = []; //массив li
@@ -17,44 +20,36 @@ for (let i = 1; i <= countOfItems; i++) {
     items.push(li);
 }
 
-let paginationSize = items.length; // pages size
-let paginationPage = 1; // selected page
-let paginationStep = 3; // pages before and after current
-
-console.log(paginationSize);
-
-
-//очистить контейнер с записями
-// slideListsWrap.innerHTML = '';
-
 //после загрузки страницы отобразить первый элемент массива items (первые три записи)
 showPage(items[0]);
 
+let item;
 //пагинация
 for (let item of items) {
     item.addEventListener('click', function (e) {
         showPage(this);
-
-        //скролл к первой записи после клика по кнопке пагинации
-        const btnScroll = e.currentTarget;
-        console.log(btnScroll);
-        const gotoBlock = document.querySelector(btnScroll.dataset.goto);
-        console.log(gotoBlock);
-        if (gotoBlock) {
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
-            window.scrollTo({
-                top: gotoBlockValue - 100,
-            });
-        }
+        scrollPage(e);
     });
+}
+
+function scrollPage(e) {
+    //скролл к первой записи после клика по кнопке пагинации
+    const btnScroll = e.currentTarget;
+    const gotoBlock = document.querySelector(btnScroll.dataset.goto);
+    if (gotoBlock) {
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+        window.scrollTo({
+            top: gotoBlockValue - 100,
+        });
+    }
 }
 
 function showPage(item) {
     //выделить активную кнопку пагинации
-    let activeLi = document.querySelector('#pagination li.current');
     if (activeLi) {
         activeLi.classList.remove('current');
     }
+    activeLi = item;
     item.classList.add('current');
 
     let pageNum = +item.innerHTML;
@@ -70,165 +65,15 @@ function showPage(item) {
     }
 }
 
-// /* * * * * * * * * * * * * * * * *
-//  * Pagination
-//  * javascript page navigation
-//  * * * * * * * * * * * * * * * * */
-//
-// var Pagination = {
-//
-//     code: '',
-//
-//     // --------------------
-//     // Utility
-//     // --------------------
-//
-//     // converting initialize data
-//     Extend: function(data) {
-//         data = data || {};
-//         Pagination.size = data.size || 300;
-//         Pagination.page = data.page || 1;
-//         Pagination.step = data.step || 3;
-//     },
-//
-//     // add pages by number (from [s] to [f])
-//     Add: function(s, f) {
-//         for (var i = s; i < f; i++) {
-//             Pagination.code += '<a>' + i + '</a>';
-//         }
-//     },
-//
-//     // add last page with separator
-//     Last: function() {
-//         Pagination.code += '<i>...</i><a>' + Pagination.size + '</a>';
-//     },
-//
-//     // add first page with separator
-//     First: function() {
-//         Pagination.code += '<a>1</a><i>...</i>';
-//     },
-//
-//
-//
-//     // --------------------
-//     // Handlers
-//     // --------------------
-//
-//     // change page
-//     Click: function() {
-//         Pagination.page = +this.innerHTML;
-//         Pagination.Start();
-//     },
-//
-//     // previous page
-//     Prev: function() {
-//         Pagination.page--;
-//         if (Pagination.page < 1) {
-//             Pagination.page = 1;
-//         }
-//         Pagination.Start();
-//     },
-//
-//     // next page
-//     Next: function() {
-//         Pagination.page++;
-//         if (Pagination.page > Pagination.size) {
-//             Pagination.page = Pagination.size;
-//         }
-//         Pagination.Start();
-//     },
-//
-//
-//
-//     // --------------------
-//     // Script
-//     // --------------------
-//
-//     // binding pages
-//     Bind: function() {
-//         let a = Pagination.e.getElementsByTagName('a');
-//         for (let i = 0; i < a.length; i++) {
-//             if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
-//             a[i].addEventListener('click', Pagination.Click, false);
-//         }
-//     },
-//
-//     // write pagination
-//     Finish: function() {
-//         Pagination.e.innerHTML = Pagination.code;
-//         Pagination.code = '';
-//         Pagination.Bind();
-//     },
-//
-//     // find pagination type
-//     Start: function() {
-//         if (Pagination.size < Pagination.step * 2 + 6) {
-//             Pagination.Add(1, Pagination.size + 1);
-//         }
-//         else if (Pagination.page < Pagination.step * 2 + 1) {
-//             Pagination.Add(1, Pagination.step * 2 + 4);
-//             Pagination.Last();
-//         }
-//         else if (Pagination.page > Pagination.size - Pagination.step * 2) {
-//             Pagination.First();
-//             Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
-//         }
-//         else {
-//             Pagination.First();
-//             Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
-//             Pagination.Last();
-//         }
-//         Pagination.Finish();
-//     },
-//
-//
-//
-//     // --------------------
-//     // Initialization
-//     // --------------------
-//
-//     // binding buttons
-//     Buttons: function(e) {
-//         var nav = e.getElementsByTagName('a');
-//         nav[0].addEventListener('click', Pagination.Prev, false);
-//         nav[1].addEventListener('click', Pagination.Next, false);
-//     },
-//
-//     // create skeleton
-//     Create: function(e) {
-//
-//         var html = [
-//             '<a class="pagination-btn-p"></a>', // previous button
-//             '<span></span>',  // pagination container
-//             '<a class="pagination-btn-n"></a>'  // next button
-//         ];
-//
-//         e.innerHTML = html.join('');
-//         Pagination.e = e.getElementsByTagName('span')[0];
-//         Pagination.Buttons(e);
-//     },
-//
-//     // init
-//     Init: function(e, data) {
-//         Pagination.Extend(data);
-//         Pagination.Create(e);
-//         Pagination.Start();
-//     }
-// };
-//
-//
-//
-// /* * * * * * * * * * * * * * * * *
-// * Initialization
-// * * * * * * * * * * * * * * * * */
-//
-// var init = function() {
-//     Pagination.Init(document.getElementById('pagination'), {
-//         size: 30, // pages size
-//         page: 1,  // selected page
-//         step: 1   // pages before and after current
-//     });
-// };
-//
-// document.addEventListener('DOMContentLoaded', init, false);
-
+prevPage.addEventListener('click', function (e) {
+    if (activeLi.previousElementSibling) {
+        showPage(activeLi.previousElementSibling);
+        scrollPage(e);
+    }
+});
+nextPage.addEventListener('click', function (e) {
+    if (activeLi.nextElementSibling) {
+        showPage(activeLi.nextElementSibling);
+        scrollPage(e);
+    }
+});
