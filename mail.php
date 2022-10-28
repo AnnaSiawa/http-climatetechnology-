b645f7930050148b80db53ed6aeb4c856dddf666
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-10-28 15:55:47 +0300

    change mail.php response to json

--- mail.php
+++ mail.php
@@ -13,19 +13,26 @@ $mail->IsHTML(true);
 $name = $_POST['name'];
 //$serName = $_POST['ser_name'];
 $phone = $_POST['phone'];
-$address = $_POST['address'];
+//$address = $_POST['address'];
 $message = $_POST['message'];
 
 $mail->setFrom('annashursh1992@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('anya.shurshalova@mail.ru');     // Кому будет уходить письмо
 $mail->isHTML(true);                                  // Set email format to HTML
 $mail->Subject = 'Заявка с сайта';
-$mail->Body = '' . $name . ' ' . ' оставил(a) заявку, номер телефона: ' . $phone . '<br>Адрес этого пользователя: ' . $address . '<br>Сообщение пользователя: ' . $message;
+$mail->Body = '' . $name . ' ' . ' оставил(a) заявку, номер телефона: ' . $phone . '<br>Адрес этого пользователя: ' . 'не указан' . '<br>Сообщение пользователя: ' . $message;
 
 $success = false;
+$result = $mail->send();
 if ($mail->send()) {
 	$success = true;
 }
-header("Location: index.php?success={$success}");
-?>
+$response = [
+	'success' => $success,
+	'result' => $result
+];
 
+header('Content-type: application/json');
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
+
+?>
